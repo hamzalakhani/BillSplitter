48339a7f6e6331db1112ffc44bfd8eec979ff810
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-13 18:14:27 -0500

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Hamza Lakhani on 2016-11-13.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Hamza Lakhani on 2016-11-13.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
