777e0448b989a78530caf458e0d32f992c5c1f45
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-13 19:15:23 -0500

    Basic done-still requires work

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *totalAmountTextField;
+@property (weak, nonatomic) IBOutlet UISlider *peopleAmountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *amountEachPersonLabel;
+@property (weak, nonatomic) IBOutlet UIButton *tapToCalculateButton;
 
 @end
 
@@ -25,5 +29,12 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+-(IBAction)calculateSplitAmout:(id)sender{
+//    float billAmount = [self.totalAmountTextField.text floatValue];//when one needs to asign the text input to a value
+//    int peopleAmount = [self.peopleAmountSlider value];
+//    float perPerson = billAmount/peopleAmount;
+    NSDecimalNumber *billamount = [NSDecimalNumber decimalNumberWithString:self.totalAmountTextField.text];
+    NSDecimalNumber *peopleAmount = [NSDecimalNumber decimalNumberWithMantissa:self.peopleAmountSlider.value exponent:0 isNegative:NO];//review this
+    self.amountEachPersonLabel.text = [NSString stringWithFormat:@"%@", [billamount decimalNumberByDividingBy:peopleAmount]];
+}
 @end
